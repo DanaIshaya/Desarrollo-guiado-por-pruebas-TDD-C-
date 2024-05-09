278402ad524517088affdcd2ad48fde54ac936bf
DanaIshaya <72181620@continental.edu.pe>
2024-05-09 00:12:25 -0500

    TDD maximo comun divisor

---
+++ PROYECTOMCD/tdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximocomundivisor
+{
+    public class tdd
+    {
+        public static int mcd(int nro1, int nro2)
+        {
+            while (nro2 != 0)
+            {
+             //Euclides
+                int x = nro2;
+
+                nro2 = nro1%nro2;
+
+                nro1 = x;
+            }
+            return nro1;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using maximocomundivisor;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void mcdTestMethod1()
+        {
+            int nro1 = 50;
+            int nro2 = 90;
+            int esperado = 10;
+            int prueba = tdd.mcd(nro1, nro2);
+            Assert.AreEqual(esperado, prueba);
+        }
+    }
+}
